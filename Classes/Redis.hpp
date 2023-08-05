8e8efa0ff496813c093f1112eb630f414528ac90
zhu-to-u <2528487925@qq.com>
2023-08-05 17:03:29 +0800

    增加了群聊客户端

--- Classes/Redis.hpp
+++ Classes/Redis.hpp
@@ -174,18 +174,19 @@ bool hexists(const string& key, const string& field) {
     }
 }
 
-// 使用 LPUSH 命令将值插入列表头部
-    bool lpushValue(const string& key, const string& value) {
-        // 发送 LPUSH 命令到 Redis 并存储回复。
-        redisReply* reply = (redisReply*)redisCommand(context, "LPUSH %s %s", key.c_str(), value.c_str());
-        if (reply != nullptr && reply->type == REDIS_REPLY_INTEGER) {
-            // 判断回复的整数值是否大于等于 0，表示插入成功。
-            bool success = (reply->integer >= 0);
-            freeReplyObject(reply);
-            return success;
-        }
-        return false; // 返回 false 表示插入失败
+// 使用 RPUSH 命令将值插入列表尾部
+    bool rpushValue(const string& key, const string& value) {
+    // 发送 RPUSH 命令到 Redis 并存储回复。
+    redisReply* reply = (redisReply*)redisCommand(context, "RPUSH %s %s", key.c_str(), value.c_str());
+    if (reply != nullptr && reply->type == REDIS_REPLY_INTEGER) {
+        // 判断回复的整数值是否大于等于 0，表示插入成功。
+        bool success = (reply->integer >= 0);
+        freeReplyObject(reply);
+        return success;
     }
+    return false; // 返回 false 表示插入失败
+}
+
 
     string lindexValue(const string& key, int index) {
     // 使用 LINDEX 命令获取列表中指定索引的元素
